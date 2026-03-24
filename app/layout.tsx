1ae5774e6e3ef1e137d6ecb80ed51b726c0f5720
skbinteriors <enquiries@skb-interiors.co.uk>
2026-03-24 14:54:57 +0000

    Login page updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import '@/styles/globals.css'
-import { CinematicBackground } from '@/components/CinematicBackground'
-
-export const metadata = {
-  title: 'TradeLife',
-  description: 'Smart finance for tradespeople',
-}
+import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <CinematicBackground />
-        <div className="relative z-[2]">{children}</div>
+      <body className="relative min-h-screen text-white">
+
+        <div className="fixed inset-0 -z-10">
+          <img src="/login-bg.png" className="w-full h-full object-cover" />
+          <div className="absolute inset-0 bg-black/30" />
+          <div className="absolute left-0 top-1/2 w-[600px] h-[600px] bg-blue-600/20 blur-[120px]" />
+          <div className="absolute right-0 bottom-0 w-[600px] h-[600px] bg-orange-500/20 blur-[120px]" />
+        </div>
+
+        {children}
+
       </body>
     </html>
   )
