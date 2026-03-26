f399bef80f25a02f43d3e2d50a78fb7956af6270
skbinteriors <enquiries@skb-interiors.co.uk>
2026-03-26 08:18:48 +0000

    resolve login merge conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="relative min-h-screen text-white">
 
         <div className="fixed inset-0 -z-10">
-          <img src="/login-bg.png" className="w-full h-full object-cover" />
           <div className="absolute inset-0 bg-black/30" />
           <div className="absolute left-0 top-1/2 w-[600px] h-[600px] bg-blue-600/20 blur-[120px]" />
           <div className="absolute right-0 bottom-0 w-[600px] h-[600px] bg-orange-500/20 blur-[120px]" />
